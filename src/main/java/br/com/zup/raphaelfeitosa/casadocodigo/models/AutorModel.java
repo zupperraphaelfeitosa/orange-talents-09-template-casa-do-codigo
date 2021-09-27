5e3f0b66f6051cb59cbf696ae2b65deee934cc45
zupperraphaelfeitosa <raphael.feitosa@zup.com.br>
2021-09-27 15:46:27 -0300

    create Autor model

---
+++ src/main/java/br/com/zup/raphaelfeitosa/casadocodigo/models/AutorModel.java
@@ -0,0 +1,37 @@
+package br.com.zup.raphaelfeitosa.casadocodigo.models;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tb_autor")
+public class AutorModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private LocalDateTime instante = LocalDateTime.now();
+
+    @Column(nullable = false, length = 400)
+    private String descricao;
+
+    @Deprecated
+    public AutorModel() {
+    }
+
+    public AutorModel(String nome, String email, String descricao) {
+        this.nome = nome;
+        this.email = email;
+        this.descricao = descricao;
+    }
+}
