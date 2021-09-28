edfce46b284d8376e1cf5185c58e8ac2385f181b
zupperraphaelfeitosa <raphael.feitosa@zup.com.br>
2021-09-28 09:00:19 -0300

    add findByEmail

--- src/main/java/br/com/zup/raphaelfeitosa/casadocodigo/repositories/AutorRepository.java
+++ src/main/java/br/com/zup/raphaelfeitosa/casadocodigo/repositories/AutorRepository.java
@@ -3,5 +3,9 @@ package br.com.zup.raphaelfeitosa.casadocodigo.repositories;
 import br.com.zup.raphaelfeitosa.casadocodigo.models.AutorModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface AutorRepository extends JpaRepository<AutorModel, Long> {
+
+    Optional<AutorModel> findByEmail(String email);
 }
